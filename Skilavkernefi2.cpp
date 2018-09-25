3b80a9a5a99c0ee70020c3203835cf0e4da797a5
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-09-25 16:58:45 +0000

    Add files via upload

--- Skilavkernefi2.cpp
+++ Skilavkernefi2.cpp
@@ -6,9 +6,16 @@
 #include <cmath>
 using namespace std;
 
-//int ip_dec(int ip_tala){
-//	int dec_tala = (ip_tala[0] * 16777216) + (ip_tala[1] * 65536) + (ip_tala[2] * 256) + (1)
-//}
+int ip_dec(int ip_tala[4]){
+	return (ip_tala[0] << 24 | ip_tala[1] << 16 | ip_tala[2] << 8 | ip_tala[3]);
+}
+
+void dec_ip(int ip_tala[4], int ipheiltala){
+	ip_tala[0] = ipheiltala >> 24 & 0xff;
+	ip_tala[1] = ipheiltala >> 16 & 0xff;
+	ip_tala[2] = ipheiltala >> 8 & 0xff;
+	ip_tala[3] = ipheiltala & 0xff;
+}
 
 
 int main(){
@@ -23,18 +30,23 @@ int main(){
 			cin >> ip_tala[i];
 		}
 		//breyta Ip tölu í heiltölu
-		unsigned int ipheiltala = ip_tala[0] << 24 | ip_tala[1] << 16 | ip_tala[2] << 8 | ip_tala[3];
+		//unsigned int ipheiltala = ip_tala[0] << 24 | ip_tala[1] << 16 | ip_tala[2] << 8 | ip_tala[3];
+		unsigned int ipheiltala = ip_dec(ip_tala);
+		cout << ipheiltala;
 		//breyta Ip tölu til baka
 		//ip_tala[0] = ipheiltala >> 24 & 0xff;
-		//ip_tala[0] = ipheiltala >> 16 & 0xff;
-		cout << ip_tala[0];
+		//ip_tala[1] = ipheiltala >> 16 & 0xff;
 		cout << "Hversu morg net? ";
 		cin >> morg_net;
 		cout << "Hversu margir bitar ";
 		cin >> margir_bitar;
 		int subnet_bitar = ceil(log2(morg_net));
-		int tala = 32-margir_bitar-subnet_bitar; 
+		int tala = 32-margir_bitar+subnet_bitar; 
 		int magic = pow(2,tala);
+		for (int i = 0; i <= morg_net; i++){
+			cout << ip_tala[0];
+
+		}
 		//int subnet0 = ipheiltala;
 		//int subnet1 = subnet0 + magic;
 		//int subnet2 = subnet1 + magic;
