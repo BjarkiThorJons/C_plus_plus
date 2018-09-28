466abb7dca20c7ff6f4f5db4bb07c14f12706cff
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-09-28 13:36:53 +0000

    Add files via upload

--- Skilavkernefi2.cpp
+++ Skilavkernefi2.cpp
@@ -1,4 +1,4 @@
-// Skilavkernefi2.cpp : Defines the entry point for the console application.
+ï»¿// Skilavkernefi2.cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
@@ -6,51 +6,72 @@
 #include <cmath>
 using namespace std;
 
-int ip_dec(int ip_tala[4]){
+int ip_dec(int *ip_tala){
 	return (ip_tala[0] << 24 | ip_tala[1] << 16 | ip_tala[2] << 8 | ip_tala[3]);
 }
 
-void dec_ip(int ip_tala[4], int ipheiltala){
+void dec_ip(int *ip_tala, int ipheiltala){
 	ip_tala[0] = ipheiltala >> 24 & 0xff;
 	ip_tala[1] = ipheiltala >> 16 & 0xff;
 	ip_tala[2] = ipheiltala >> 8 & 0xff;
 	ip_tala[3] = ipheiltala & 0xff;
 }
+void synaIpTolu(const char*texti, int *ipTolur) {
+	for (int i = 0; i < 4; i++) {
+		if (i != 3) {
+			cout << ipTolur[i] << ".";
+		}
+		else {
+			cout << ipTolur[i] << "       ";
+		}
+	}
+}
 
+void synaIpTolu(const char*texti, int ipTala) {
+	int ip[4];
+	dec_ip(ip, ipTala);
+	synaIpTolu(texti, ip);
+}
+void subnetting(int ipNet, int netBitar, int bitaFjoldi) {
+	cout << "IP net        Fyrsta nothaefa      Sidasta nothaefa         Broadcast       Maski" << endl;
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
+	int magic = pow(2, (32 - (netBitar + bitaFjoldi))); //magic number
+	for (int i = 0; i < pow(2, bitaFjoldi); i++) {
+		int tempnet = ipNet + (magic*i);
+		synaIpTolu("", tempnet); //network
+		synaIpTolu("\t", tempnet + 1); //fyrsta nothaefa
+		synaIpTolu("\t", tempnet + magic - 2); //seinasta nothaefa
+		synaIpTolu("\t", tempnet + magic - 1); //broadcast
+		tempnet = 0xffffffff << (32 - (netBitar + bitaFjoldi)); //subnet maskinn 1
+		synaIpTolu("\t", tempnet); //subnet maskinn 2
+		cout << endl;
+	}
+
+}
 
 int main(){
 	int val = 1;
 	while (val){
-		//láta notanda skrifa ip tölu og láta hana í lista
-		int ip_tala[4];
-		int morg_net;
-		int margir_bitar;
+		//lÃ¡ta notanda skrifa ip tÃ¶lu og lÃ¡ta hana Ã­ lista
+		int *ip_tala = new int[4];
+		int morg_net, margir_bitar, subnet_bitar;
 		cout << "Sladu inn ip tolu ";
 		for (int i = 0; i < 4; i++){
 			cin >> ip_tala[i];
 		}
-		//breyta Ip tölu í heiltölu
+		//breyta Ip tÃ¶lu Ã­ heiltÃ¶lu
 		//unsigned int ipheiltala = ip_tala[0] << 24 | ip_tala[1] << 16 | ip_tala[2] << 8 | ip_tala[3];
-		unsigned int ipheiltala = ip_dec(ip_tala);
-		cout << ipheiltala;
-		//breyta Ip tölu til baka
+		//breyta Ip tÃ¶lu til baka
 		//ip_tala[0] = ipheiltala >> 24 & 0xff;
 		//ip_tala[1] = ipheiltala >> 16 & 0xff;
 		cout << "Hversu morg net? ";
 		cin >> morg_net;
 		cout << "Hversu margir bitar ";
 		cin >> margir_bitar;
-		int subnet_bitar = ceil(log2(morg_net));
-		int tala = 32-margir_bitar+subnet_bitar; 
-		int magic = pow(2,tala);
-		for (int i = 0; i <= morg_net; i++){
-			cout << ip_tala[0];
+		int ip2Dec = ip_dec(ip_tala);
+		subnet_bitar = ceil(log2(morg_net));
+		subnetting(ip2Dec, margir_bitar, subnet_bitar);
 
-		}
-		//int subnet0 = ipheiltala;
-		//int subnet1 = subnet0 + magic;
-		//int subnet2 = subnet1 + magic;
-		cout << subnet_bitar;
 	}
 	return 0;
 }
