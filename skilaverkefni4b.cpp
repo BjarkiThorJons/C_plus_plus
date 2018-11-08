bb29df3dfceba72dcce4800cfee9defc217cc885
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-11-08 12:47:39 +0000

    Update skilaverkefni4b.cpp

--- skilaverkefni4b.cpp
+++ skilaverkefni4b.cpp
@@ -66,36 +66,48 @@ public:
 
 class FlightList {
 private:
-	int *listi;
-	int staflaBendill;
+	FlightBooking  *listi;
+	int next;
 	int staerd;
 public:
 	FlightList() {
-		staerd = 2;
-		listi = new int[staerd];
-		staflaBendill = 0;
+		this->staerd = 2;
+		listi = new FlightBooking[staerd];
+		next = 0;
 	}
-	void setjaStak(int stak) {
-		if (staflaBendill <= staerd - 1)
-			listi[staflaBendill++] = stak;
+	void setja(FlightBooking stak) {
+		if (next <= staerd - 1)
+			listi[next++] = stak;
 		else {
-			int *temp = new int[staerd * 2];
+			FlightBooking *temp = new FlightBooking[staerd * 2];
 			for (int i = 0; i < staerd; i++)
 				temp[i] = listi[i];
 
 			delete[]listi;
 			listi = temp;
 			staerd *= 2;
-			listi[staflaBendill++] = stak;
+			listi[next++] = stak;
 		}
 	}
-	int saekjaStak() {
-		return listi[--staflaBendill];
+	int saekja() {
+		for (int i = 0; i < sizeof(listi); i++){
+			listi[i].Booking();
+		}
+		
+		return 0;
 	}
-	void synaStafla() {
-		for (int i = 0; i < staflaBendill; i++)
-			cout << listi[i] << " ";
-		cout << endl << endl;
+	int id(int b){
+		for (int i = 0; i < sizeof(listi); i++)
+		{
+			//cout << endl << booking[i].returnId() <<" " << id << endl;
+			if (listi[i].returnId() == b) {
+				return i;
+			}
+		}
+	}
+	void syna() {
+		for (int i = 0; i < next; i++)
+			listi[i].Booking();
 	}
 	~FlightList() {
 		delete[]listi;
@@ -115,6 +127,12 @@ int main() {
 	s.Booking();
 	*/
 	FlightBooking booking[10];
+	FlightList boooking = FlightList();
+	boooking.setja(FlightBooking(1, 200, 0));
+	boooking.setja(FlightBooking(2, 400, 0));
+	boooking.setja(FlightBooking(3, 600, 0));
+	boooking.setja(FlightBooking(4, 800, 0));
+	cout << "asd: " << boooking.saekja();
 	string command = "";
 	int n;
 	int id;
@@ -131,13 +149,16 @@ int main() {
 		ss << command;
 		cout << endl;
 		ss >> command >> id >> n;
-		for (int i = 0; i < 10; i++)
+		/*
+		for (int i = 0; i < sizeof(booking); i++)
 		{
 			//cout << endl << booking[i].returnId() <<" " << id << endl;
 			if (booking[i].returnId() == id) {
 				s = i;
+				cout << s;
 			}
 		}
+		*/
 		if (command == "add") {
 			booking[s].reserveSeats(n);
 		}
